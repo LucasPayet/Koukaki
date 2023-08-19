ec702641a2ec2780d5cdd08fb4f8dd647598f40e
LucasPayet <lucas.payet.william@gmail.com>
2023-08-19 16:45:42 +0400

    temlpates partiels

--- front-page.php
+++ front-page.php
@@ -67,12 +67,7 @@ get_header();
             </div>
             </section>
 
-        <section id="oscars">
-            <div>
-                <span><p>Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</p></span>
-                <img width="278px" height="129px" src="http://localhost/Koukaki/wp-content\themes\foce-child\assets\image\18-courts-metrages-francais-d-animation-eligibles-aux-oscars-2021 1.png">
-            </div>
-        </section>
+        <?php get_template_part( 'newsletter' ); ?>
     </main><!-- #main -->
 
 <?php
---
+++ oscars.php
@@ -0,0 +1,6 @@
+<section id="oscars">
+    <div>
+        <span><p>Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</p></span>
+        <img width="278px" height="129px" src="http://localhost/Koukaki/wp-content\themes\foce-child\assets\image\18-courts-metrages-francais-d-animation-eligibles-aux-oscars-2021 1.png">
+    </div>
+</section>
